b60503287407ffa87b275d35398517222b4ffdd6
viktor-vvd <davidenko3015@gmail.com>
2023-09-06 18:13:50 +0300

    add PopularProducts

---
+++ src/components/Home/PopularProducts.js
@@ -0,0 +1,93 @@
+import React from "react";
+import images from "imports/ImagesImport";
+import ProductCard from "components/base/ProductCard";
+
+const PopularProducts = () => {
+  const data = [
+    {
+      id: 0,
+      image: images["product1.png"],
+      name: "Ball for a dog «Denta Fun» d=5 см (rubber)",
+      rate: 5,
+      comments: 2,
+      discount: 0,
+      price: 4,
+      new: false,
+    },
+    {
+      id: 1,
+      image: images["product2.png"],
+      name: "Dog Toys Big Dog barbell Chew toy Interactive Stress Relief Dog Toy",
+      rate: 5,
+      comments: 5,
+      discount: 50,
+      price: 10,
+      new: false,
+    },
+    {
+      id: 2,
+      image: images["product3.png"],
+      name: "Mammoth Cottonblend 3 Knot Dog Rope Toy, Color Varies, Large",
+      rate: 5,
+      comments: 10,
+      discount: 0,
+      price: 8.5,
+      new: true,
+    },
+    {
+      id: 3,
+      image: images["product4.png"],
+      name: "Frisco Bird Teaser with Feathers Cat Toy",
+      rate: 5,
+      comments: 10,
+      discount: 0,
+      price: 3.42,
+      new: false,
+    },
+  ];
+
+  return (
+    <section className="container-vertical outer__container section-popular">
+      <div className="container-vertical container section-popular__container">
+        <h2 className="title">Popular products</h2>
+        <div className="container-horisontal populars">
+
+        {data &&
+          data.map((item, index) => (
+            <ProductCard item={item} key={index} />
+          ))}         
+        </div>
+      </div>
+      <div className="container-horisontal for-cat-dog__container">
+        <div className="container-horisontal for-cat-dog__button for-cat-dog__button_cat">
+          <span className="text__button">
+            Products for <span className="text__button cat">CAT</span>
+          </span>
+          <img
+            className="for-cat-dog__button__icon"
+            src={images["caticon.png"]}
+            alt="cat"
+          />
+          <img
+            className="button__icon_arrow button__icon_arrow_right"
+            src={images["toprightarrow.png"]}
+            alt="arrow"
+          />
+        </div>
+        <div className="container-horisontal for-cat-dog__button for-cat-dog__button_dog">
+          <span className="text__button">
+            Products for <span className="text__button dog">DOG</span>
+          </span>
+          <img src={images["dogicon.png"]} alt="arrow" />
+          <img
+            className="button__icon_arrow button__icon_arrow_right"
+            src={images["toprightarrow.png"]}
+            alt="arrow"
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default PopularProducts;
---
+++ src/components/base/ProductCard.js
@@ -0,0 +1,58 @@
+import React from "react";
+import images from "imports/ImagesImport";
+
+const ProductCard = ({ item }) => {
+  return (
+    <div className="container-vertical product-card">
+      <div className="container-horisontal image__container">
+          {item.discount > 0 && (
+            <span class="tag tag_discount">-{item.discount}%</span>
+          )}
+          {item.new && (
+            <span class="tag tag_new">New</span>
+          )}
+        <img className="image" src={item.image} alt="product img" />
+      </div>
+      <h3 className="text product-card__title">{item.name}</h3>
+      <div className="container-horisontal rate">
+        <div className="container-horisontal rate__stars">
+          {item.rate &&
+            [...Array(item.rate)].map((e, i) => (
+              <img
+                className="stars__item"
+                src={images["star.png"]}
+                alt="star"
+                key={"star" + i}
+              />
+            ))}
+        </div>
+        <span className="rate__text">{item.comments}</span>
+      </div>
+      <div className="container-horisontal product-card__bottom">
+        <div className="container-horisontal price__container">
+          {item.discount > 0 && (
+            <span className="price price_new">
+              $
+              {Math.round(item.price * (1 - item.discount / 100))
+                .toString()
+                .replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, "$1 ")}{" "}
+            </span>
+          )}
+          <span className={"price" + (item.discount > 0 ? " price_old" : "")}>
+            ${item.price}
+          </span>
+        </div>
+        <div className="container-horisontal button">
+          <span className="button__text">+</span>
+          <img
+            className="button__icon"
+            src={images["basket.png"]}
+            alt="basket"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
